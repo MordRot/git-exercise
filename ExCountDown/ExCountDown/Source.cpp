2903cbcbe52f7b07cefafddf78bbc1683d9f1962
mord <mord@cyberElite.jct>
2017-10-27 15:46:46 +0300

    write Git 1 Exercise

--- ExCountDown/ExCountDown/Source.cpp
+++ ExCountDown/ExCountDown/Source.cpp
@@ -4,30 +4,5 @@ using namespace std;
 
 void main()
 {
-	cout << "" << "Time until takeoff: 12";
-	
-	for (int i = 12; i > 0; i--)
-	{
-		cout << "\b\b";
-		if (i > 9)
-		{
-			cout << i;
-		}
-		else
-		{			
-			cout << i << " ";
-		}
-		this_thread::sleep_for(1s);
-	}
-	system("cls");
-
-	cout << "\r";
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "Takeoff";
-		this_thread::sleep_for(1s);
-		system("cls");
-		this_thread::sleep_for(1s);
-	}
-	
+	cout << "Hello, world!" << endl;
 }
